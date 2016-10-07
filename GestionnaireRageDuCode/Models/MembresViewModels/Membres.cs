6fad5b01706973bd26a18e60c86d9a66557dbcd9
Timothé Provost <timothe.provost@gmail.com>
2016-10-07 14:28:45 -0400

    Commit pour pull

--- GestionnaireRageDuCode/Models/MembresViewModels/Membres.cs
+++ GestionnaireRageDuCode/Models/MembresViewModels/Membres.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,17 @@ namespace GestionnaireRageDuCode.Models.MembresViewModels
 {
     public class Membres
     {
+        public enum ROLE
+        {
+            Membre,
+            Modérateur,
+            Administrateur,
+            Fondateur
+        }
 
+        public int ID { get; set; }
+        public string Prenom { get; set; }
+        public string Nom { get; set; }
+        public int Role { get; set; }
     }
 }
