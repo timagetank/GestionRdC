a8751fe7fb85de03d1156e847e8852bab89e9125
Constant <alexandre.constant@outlook.com>
2016-10-07 14:07:57 -0400

    Début de modèles

---
+++ GestionnaireRageDuCode/Models/ForumViewModels/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestionnaireRageDuCode.Models.ForumViewModels
+{
+    public class Message
+    {
+        public int ID { get; set; }
+        public string Corps { get; set; }
+        public DateTime DateCreation { get; set; }
+        // Auteur
+    }
+}
---
+++ GestionnaireRageDuCode/Models/ForumViewModels/Sujet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestionnaireRageDuCode.Models.ForumViewModels
+{
+    public class Sujet
+    {
+        public int ID { get; set; }
+        public string Titre { get; set; }
+        public string Description { get; set; }
+        // Auteur
+        public DateTime DateCreation {get; set;}
+        public DateTime DateDernierMessage { get; set; }
+        // Dernier usager // Auteur du dernier message
+        public int NbPost { get; set; }
+    }
+}
